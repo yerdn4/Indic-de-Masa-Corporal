1e262ceecea435e4c27e5582a3a223fb2d33a5d1
yerdn4 <24470088@campeche.tecnm.mx>
2025-10-02 23:19:58 -0600

    Delete Layout.Stackl.ayou directory

--- Layout.Stackl.ayou/Layout.Stackl.ayou/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Layout.Stackl.ayou")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Layout.Stackl.ayou.Pages")]
--- Layout.Stackl.ayou/Layouts.Grid/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Layouts.Grid")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Layouts.Grid.Pages")]
